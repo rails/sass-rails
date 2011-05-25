98909a807f0b3c41a98df08da50d9370e54d28ab
Chris Eppstein <chris@eppsteins.net>
2011-05-25 08:01:34 -0700

    Change config.sass.syntax to config.sass.preferred_syntax to avoid conflicts with the syntax option in Sass and to indicate that it's only a preference for generation, but both syntaxes can be used within a project if needed.

--- lib/sass-rails/railtie.rb
+++ lib/sass-rails/railtie.rb
@@ -1,14 +1,14 @@
 module Sass::Rails
   class Railtie < ::Rails::Railtie
     config.sass = ActiveSupport::OrderedOptions.new
-    config.sass.syntax = :scss
+    config.sass.preferred_syntax = :scss
 
     initializer :setup_sass do |app|
-      syntax     = app.config.sass.syntax
-      alt_syntax = syntax.to_s == "sass" ? "scss" : "sass"
+      syntax     = app.config.sass.preferred_syntax.to_sym
+      alt_syntax = syntax == :sass ? "scss" : "sass"
 
       app.config.generators.hide_namespace alt_syntax
-      config.app_generators.stylesheet_engine app.config.sass.syntax
+      config.app_generators.stylesheet_engine syntax
     end
 
     initializer :setup_compression do |app|
