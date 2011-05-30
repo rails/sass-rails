9f0f28d7ba9aa73376c613035dac91569f00738b
Chris Eppstein <chris@eppsteins.net>
2011-05-30 16:28:49 -0700

    Revert commit 11b2580f3317bf2a944df80ee4a642cf257270f2

--- lib/rails/generators/sass_scaffold.rb
+++ lib/rails/generators/sass_scaffold.rb
@@ -8,7 +8,7 @@ module Sass
         dir = ::Rails::Generators::ScaffoldGenerator.source_root
         file = File.join(dir, "scaffold.css")
         converted_contents = ::Sass::CSS.new(File.read(file)).render(syntax)
-        create_file "app/assets/stylesheets/#{name}.css.#{syntax}", converted_contents
+        create_file "app/assets/stylesheets/scaffolds.css.#{syntax}", converted_contents
       end
     end
   end
