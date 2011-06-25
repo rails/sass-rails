b7bde864b6938c626df0cfd1bda84fe885d9f32f
Chris Eppstein <chris@eppsteins.net>
2011-06-24 23:05:15 -0700

    Future-proof against some upcoming changes to the Sprocket's packaging.

--- lib/sass/rails/monkey_patches.rb
+++ lib/sass/rails/monkey_patches.rb
@@ -1,20 +1,36 @@
-require 'sprockets/railtie'
+module Sass::Rails::SassContext
+  attr_accessor :sass_config
+end
 
-module Sprockets
-  class Railtie < ::Rails::Railtie
+module Sass::Rails::SprocketsConfig
+  def self.included(base)
+    base.alias_method_chain :asset_environment, :sass_config
+  end
 
-    module SassContext
-      attr_accessor :sass_config
+  def asset_environment_with_sass_config(app, *args)
+    env = asset_environment_without_sass_config(app, *args)
+    env.context_class.extend(Sass::Rails::SassContext)
+    env.context_class.sass_config = app.config.sass
+    env
+  end
+end
+
+begin
+  # Before sprockets was extracted from rails
+  require 'sprockets/railtie'
+  module Sprockets
+    class Railtie < ::Rails::Railtie
+      include Sass::Rails::SprocketsConfig
     end
-
-    protected
-
-    def asset_environment_with_sass_config(app, *args)
-      env = asset_environment_without_sass_config(app, *args)
-      env.context_class.extend(SassContext)
-      env.context_class.sass_config = app.config.sass
-      env
+  end
+rescue LoadError
+  # After sprockets was extracted into sprockets-rails
+  require 'sprockets/rails/railtie'
+  module Sprockets
+    module Rails
+      class Railtie < ::Rails::Railtie
+        include Sass::Rails::SprocketsConfig
+      end
     end
-    alias_method_chain :asset_environment, :sass_config
   end
 end
