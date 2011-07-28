ad73d00ea9f267139d3cf31231b3ca73aa69854d
Santiago Pastorino <santiago@wyeworks.com>
2011-07-28 15:53:04 -0300

    Revert "Fixes #22"
    This reverts commit 8469699c4191d21742a3315f7c452e0e069e63fc.

--- lib/sass/rails/railtie.rb
+++ lib/sass/rails/railtie.rb
@@ -37,8 +37,8 @@ module Sass::Rails
         # Display a stack trace in the css output when in development-like environments.
         config.sass.full_exception = app.config.consider_all_requests_local
       end
-      app.config.assets.context_class.extend(SassContext)
-      app.config.assets.context_class.sass_config = app.config.sass
+      app.assets.context_class.extend(SassContext)
+      app.assets.context_class.sass_config = app.config.sass
     end
 
     initializer :setup_compression do |app|
