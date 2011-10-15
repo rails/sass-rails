428503d6b0ea9c3159e1a11dcc13b9b67c615ea5
Chris Eppstein <chris@eppsteins.net>
2011-10-15 12:57:11 -0700

    Make it easier to test against gem dependencies that are bundled using a development path. All that is needed now is to add them to the top-level Gemfile.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,20 +9,19 @@ require 'mocha'
 
 Rails.backtrace_cleaner.remove_silencers!
 
-# Set locations of local gems in this hash.
-# They will be added/replaced in the generated Gemfiles
-# You should also set them in the local Gemfile
-$gem_locations = {
-  "sass-rails" => File.expand_path("../../", __FILE__)
-}
 
-# Uncomment this if you need to test against a local rails checkout
-# $gem_locations["rails"] = "/Users/chris/Projects/rails"
-# Uncomment this if you need to test against a local sass checkout
-# $gem_locations["sass"] = "/Users/chris/Projects/sass"
-# Uncomment this if you need to test against a local sprockets checkout
-# $gem_locations["sprockets"] = "/Users/chris/Projects/sprockets"
-
+# If developing against local dependencies, this code will ensure they get picked up
+# in the project fixtures that have their own bundle environment
+$gem_locations = {}
+possible_dev_dependencies = %w(sass-rails sass rails actionpack railties sprockets)
+Bundler.load.specs.each do |s|
+  if possible_dev_dependencies.include?(s.name)
+     gem_path = s.full_gem_path
+     if File.exists?("#{gem_path}/#{s.name}.gemspec")
+       $gem_locations[s.name] = gem_path
+     end
+  end
+end
 
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
