3a7b1e4ed3a09f11df72209fb051370231a53c2c
Rafael Mendonça França <rafael.franca@plataformatec.com.br>
2012-10-16 14:08:14 -0300

    Remove the warnings from the output of the tests

--- test/fixtures/scss_project/config/environments/development.rb
+++ test/fixtures/scss_project/config/environments/development.rb
@@ -6,6 +6,8 @@ ScssProject::Application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  config.eager_load = false
+
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
