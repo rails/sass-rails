426b1c31f1ff86fe11e6b18e012c1e8a0f830efe
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2013-04-19 03:04:29 -0500

    fix eager_load and remove assets.compress config from fixtures

--- test/fixtures/sass_project/config/environments/development.rb
+++ test/fixtures/sass_project/config/environments/development.rb
@@ -6,6 +6,8 @@ ScssProject::Application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  config.eager_load = false
+
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
@@ -18,7 +20,4 @@ ScssProject::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
-
-  # Do not compress assets
-  config.assets.compress = false
 end
--- test/fixtures/scss_project/config/environments/production.rb
+++ test/fixtures/scss_project/config/environments/production.rb
@@ -4,15 +4,14 @@ ScssProject::Application.configure do
   # Code is not reloaded between requests
   config.cache_classes = true
 
+  config.eager_load = true
+
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
-
-  # Compress JavaScripts and CSS
-  config.assets.compress = true
 
   # Specify the default JavaScript compressor
   config.assets.js_compressor  = :uglifier
