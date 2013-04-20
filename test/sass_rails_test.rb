bd7b17044223f0d1add2a4073f39e89955337bc7
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2013-04-20 12:54:57 -0500

    split generators tests

--- test/sass_rails_test.rb
+++ test/sass_rails_test.rb
@@ -88,59 +88,6 @@ END_OF_COMMAND
     end
   end
 
-  test "scss files are generated during scaffold generation of scss projects" do
-    within_rails_app "scss_project" do
-      generate_scaffold
-      assert_file_exists "app/assets/stylesheets/foos.css.scss"
-      assert_file_exists "app/assets/stylesheets/scaffolds.css.scss"
-      assert_not_output /conflict/
-    end
-  end
-
-  test "sass files are generated during scaffold generation of sass projects" do
-    within_rails_app "sass_project" do
-      generate_scaffold
-      assert_file_exists "app/assets/stylesheets/foos.css.sass"
-      assert_file_exists "app/assets/stylesheets/scaffolds.css.sass"
-      assert_not_output /conflict/
-    end
-  end
-
-  test "scss files are generated during scaffold generation of a engine project, if is called with --stylesheet-engine=scss" do
-    within_rails_app "engine_project" do
-      generate_scaffold "--stylesheet-engine=scss"
-      assert_file_exists "app/assets/stylesheets/engine_project/foos.css.scss"
-      assert_file_exists "app/assets/stylesheets/scaffolds.css.scss"
-      assert_not_output /conflict/
-    end
-  end
-
-  test "sass files are generated during scaffold generation of a engine project, if is called with --stylesheet-engine=sass" do
-    within_rails_app "engine_project" do
-      generate_scaffold "--stylesheet-engine=sass"
-      assert_file_exists "app/assets/stylesheets/engine_project/foos.css.sass"
-      assert_file_exists "app/assets/stylesheets/scaffolds.css.sass"
-      assert_not_output /conflict/
-    end
-  end
-
-  # DISABLED because we've removed the feature for now.
-  # test "scss template has correct dasherized css class for namespaced controllers" do
-  #   within_rails_app "scss_project" do
-  #     runcmd "rails generate controller foo/bar"
-  #     assert_file_exists "app/assets/stylesheets/foo/bar.css.scss"
-  #     assert_match /\.foo-bar/, File.read("app/assets/stylesheets/foo/bar.css.scss")
-  #   end
-  # end
-  #
-  # test "sass template has correct dasherized css class for namespaced controllers" do
-  #   within_rails_app "sass_project" do
-  #     runcmd "rails generate controller foo/bar"
-  #     assert_file_exists "app/assets/stylesheets/foo/bar.css.sass"
-  #     assert_match /\.foo-bar/, File.read("app/assets/stylesheets/foo/bar.css.sass")
-  #   end
-  # end
-
   test "sprockets require works correctly" do
     css_output = sprockets_render("scss_project", "css_application.css")
     assert_match /globbed/, css_output
@@ -193,9 +140,5 @@ div {
   color: red;
 }
 CSS
-  end
-
-  def generate_scaffold(args = nil)
-    runcmd "bundle exec rails generate scaffold foo #{args}"
   end
 end
