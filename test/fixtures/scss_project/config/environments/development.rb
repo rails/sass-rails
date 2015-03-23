2950563fcab9c3e57ccafc4b8acfc15b9c4c9484
Joshua Peek <josh@joshpeek.com>
2015-03-23 11:28:08 -0700

    Revert "Disable asset digests"
    This reverts commit 351397609bdd96ff69ae5c005e0668a55df90a39.

--- test/fixtures/scss_project/config/environments/development.rb
+++ test/fixtures/scss_project/config/environments/development.rb
@@ -12,8 +12,6 @@ ScssProject::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  config.assets.digest = false
-
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
