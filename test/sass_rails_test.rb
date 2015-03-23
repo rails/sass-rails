dc0fab50f8531cf56ec9f1daf946156cee72c77a
Joshua Peek <josh@joshpeek.com>
2015-03-22 21:00:28 -0700

    ALways request asset with expected format

--- test/sass_rails_test.rb
+++ test/sass_rails_test.rb
@@ -105,7 +105,7 @@ class SassRailsTest < Sass::Rails::TestCase
   end
 
   test 'sass imports work correctly' do
-    css_output = sprockets_render('scss_project', 'application.scss')
+    css_output = sprockets_render('scss_project', 'application.css')
     assert_match /main/,                     css_output
     assert_match /top-level/,                css_output
     assert_match /partial-sass/,             css_output
@@ -167,7 +167,7 @@ class SassRailsTest < Sass::Rails::TestCase
   end
 
   test 'sass asset paths work' do
-    css_output = sprockets_render('scss_project', 'application.scss')
+    css_output = sprockets_render('scss_project', 'application.css')
     assert_match %r{asset-path:\s*"/assets/rails.png"},                           css_output, 'asset-path:\s*"/assets/rails.png"'
     assert_match %r{asset-url:\s*url\(/assets/rails.png\)},                       css_output, 'asset-url:\s*url\(/assets/rails.png\)'
     assert_match %r{image-path:\s*"/assets/rails.png"},                           css_output, 'image-path:\s*"/assets/rails.png"'
