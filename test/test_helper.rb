abedcfdaa322bd3bbd782f55d205a792203a935e
Lucas Mazza <lucastmazza@gmail.com>
2016-04-25 12:02:21 -0300

    Fix warnings from the test suite.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -16,7 +16,7 @@ Bundler.load.specs.each do |s|
   if possible_dev_dependencies.include?(s.name)
      gem_path = s.full_gem_path
      gem_options = { version: s.version }
-     gem_options[:path] = gem_path if File.exists?("#{gem_path}/#{s.name}.gemspec")
+     gem_options[:path] = gem_path if File.exist?("#{gem_path}/#{s.name}.gemspec")
      $gem_options[s.name] = gem_options
   end
 end
