d8099007b74558f684750618ea0ad35e384b65b0
Guillermo Iguaran <guilleiguaran@gmail.com>
2019-03-24 17:26:55 -0500

    Recomend to use SassC::Rails::Importer to users requiring sass/rails/importer

--- lib/sass/rails/importer.rb
+++ lib/sass/rails/importer.rb
@@ -1,1 +1,5 @@
-require 'sassc/rails/importer'
+# frozen_string_literal: true
+
+require "active_support/deprecation"
+
+ActiveSupport::Deprecation.warn "Sass::Rails::Importer has been removed, please use SassC::Rails::Importer instead."
